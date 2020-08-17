eced1fb037246888ec88dde240899d246f3f45b9
boss29024 <boss29024@gmail.com>
2020-08-17 15:24:29 +0700

    Test git

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo test',
       theme: ThemeData(
         // This is the theme of your application.
         //
